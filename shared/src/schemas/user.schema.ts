14a54092e0a72ed7575f2c9bb48023a0b70b8d33
Kacper Góra <kacpergoora@gmail.com>
2026-01-15 09:22:53 +0100

    Initial config

---
+++ shared/src/schemas/user.schema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const createUserSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.email(),
+  password: z.string().min(8),
+});
+
+export type CreateUserDto = z.infer<typeof createUserSchema>;
