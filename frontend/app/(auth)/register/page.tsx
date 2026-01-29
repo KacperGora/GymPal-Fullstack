7cf879eef9110c261503dac7a32e884ae932383a
Kacper Góra <kacpergoora@gmail.com>
2026-01-29 07:04:14 +0100

    feat: init for next app

---
+++ frontend/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import RegisterForm from './components/RegisterForm';
+
+export default function RegisterPage() {
+  return (
+    <div style={{ maxWidth: 400, margin: '50px auto' }}>
+      <h1>Register</h1>
+      <RegisterForm />
+    </div>
+  );
+}
